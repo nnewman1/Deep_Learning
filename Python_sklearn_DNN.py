cacc4f40dd97936de05392eef8769d29be3a1d1f
Nicholas Newman <nick.newman95@yahoo.com>
2019-10-18 21:51:40 -0600

    Update Python_sklearn_DNN.py

--- Python_sklearn_DNN.py
+++ Python_sklearn_DNN.py
@@ -1,6 +1,7 @@
 # Python tutorial using scikit-learn with a multi-hidden layer Artificial Deep Neural Network (DNN) on the iris dataset
 # Deep learning is an artificial intelligence function that imitates the workings of the human brain in processing data and creating patterns for use in decision making.
 # Deep learning is a subset of machine learning in artificial intelligence (AI) that has networks capable of learning unsupervised from data that is unstructured or unlabeled.
+# sci-kit learn or sklearn is an high-level machine learning library for python.
 
 # Import python libraries
 from sklearn import datasets
